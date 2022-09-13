bcb98b50f62b7906337f4610c00c4f9d0b00ad5e
Captressketh001 <shininglite2012@gmail.com>
2022-09-13 13:46:36 -0700

    updated task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,7 +41,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((z / 100) + '0');
-					_putchar((z /10) % 10) + '0');
+					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
 			}
