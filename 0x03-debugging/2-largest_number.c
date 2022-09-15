c65620e641ecdfb8b980ed91122e253a87487a58
Captressketh001 <shininglite2012@gmail.com>
2022-09-15 05:35:30 -0700

    updated task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	if (b > a && a > c)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
-	if (c > a && c > b)
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
