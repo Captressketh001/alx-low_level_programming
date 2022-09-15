5aae59940787ea6bc67f695d61ab33862949e8d3
Captressketh001 <shininglite2012@gmail.com>
2022-09-15 14:00:58 -0700

    task 0 and 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @c: integer expected
+ * Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for a digit (0 through 9).
+ * @c: integer expected
+ * Return: 1 if c is a digit
+ * 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 58);
+}
