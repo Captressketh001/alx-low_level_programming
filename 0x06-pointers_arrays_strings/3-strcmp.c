183e5b08db8d82278eedd3fbdb06627a65874d32
Captressketh001 <shininglite2012@gmail.com>
2022-09-22 09:18:42 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++)
+	while (*s1 == *s2)
 	{
-		return (0);
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-
 	return (*s1 - *s2);
 
 }
