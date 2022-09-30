38af1ea93d244784580656fa6cced9e7505a36d7
Captressketh001 <shininglite2012@gmail.com>
2022-09-30 04:20:19 -0700

    removed space before parenthesis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Return: 0
  */
 
-int main(int argc __attribute__((__unused__)), char *argv[] )
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
