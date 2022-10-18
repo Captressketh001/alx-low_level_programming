8473e1b5389829877c8a3ad744a2dcef034bfad8
Captressketh001 <shininglite2012@gmail.com>
2022-10-18 06:53:59 -0700

    correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (!strg)
 			strg = "(nil)";
-		else if (!separator)
+		if (!separator)
 			printf("%s", strg);
 		else if (separator && i == 0)
 			printf("%s", strg);
