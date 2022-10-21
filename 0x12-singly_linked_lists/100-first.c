c45a010eafedb56602d7484979b68b2348c93080
Captressketh001 <shininglite2012@gmail.com>
2022-10-21 16:01:48 -0700

    task 4, 5, 6

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * turtle - prints a sentence before main
+ * function is executed
+ */
+
+void turtle(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
