11174289bacf9e8b8e57d61b1ffa09531d153c52
Captressketh001 <shininglite2012@gmail.com>
2022-10-26 05:30:05 -0700

    correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,8 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *temp;
+	listint_t *temp = head;
 	unsigned int i = 0;
-
-	*temp = head;
 
 	while (temp && i < index)
 	{
