de220ce5b6bfc5eda8c9d7e6a3629a134bf68e47
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-14 08:33:01 +0100

    completed linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,5 +11,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-
+    if (array == NULL)
+    {
+        return -1;
+    }
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%d] = %d\n", i, array[i])
+        if (array[i] == value)
+            return i;
+    }
+    return -1
 }
