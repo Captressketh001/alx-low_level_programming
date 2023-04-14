1323ada9b154fee9a937bdb43e04eb654bf6a808
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-14 08:45:16 +0100

    changed the position of i to avoid error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,11 +11,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+    size_t i;
+    
     if (array == NULL)
     {
         return -1;
     }
-    size_t i;
+
     for (i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = %d\n", i, array[i]);
